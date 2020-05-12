d60b00c71a00a68551967ee36f8de0a34ae2135b
mvangin <43646116+mvangin@users.noreply.github.com>
2020-05-12 14:03:13 -0700

    add gameboard module

--- scripts/script.js
+++ scripts/script.js
@@ -1,42 +1,63 @@
-const boardSquares = document.querySelectorAll('.boardSquare');
-const playerChoice = document.querySelector('.playerChange')
-let currentPlayer = "player1";
-
-
 
 var gameBoard = (function () {
+    const boardSquares = document.querySelectorAll('.boardSquare');
+    const playerChoice = document.querySelector('.playerChange')
+    const startGame = document.querySelector('.startGame')
+    const playerTitle = document.querySelector('.playerTitle');
+
+
     var gameBoard = [];
+
+    var player = (playerName) => {
+        const nameText = playerName + " is now taking their turn";
+        return { playerName, nameText};
+    }
+
+    const player1 = player("player1");
+    const player2 = player("player2")
+    let currentPlayer = player1;
+
+
+    startGame.addEventListener("click", () => {
+        startGame.style.display = "none";
+        playerTitle.textContent = player1.nameText;
+        playerColor();
+    });
 
     playerChoice.addEventListener("click", () => {
         changePlayer();
         playerColor()
     });
 
-    playerColor();
+    function changePlayer() {
+        if (currentPlayer == player1) {
+            currentPlayer = player2;
+            playerTitle.textContent = player2.nameText;
+        } else {
+            currentPlayer = player1;
+            playerTitle.textContent = player1.nameText;
+
+        }
+        playerColor();
+    }
 
     function playerColor() {
         boardSquares.forEach((item) => {
             item.addEventListener("click", (e) => {
-                boardSquarePress(e, currentPlayer);
+                boardSquarePress(e);
             });
         });
     }
+
+    function boardSquarePress(e) {
+        if (currentPlayer == player1) {
+            e.target.textContent = "X";
+        } else {
+            e.target.textContent = "O";
+        }
+    }
+
+
 }());
 
 
-function boardSquarePress(e) {
-    if (currentPlayer == "player1") {
-        e.target.style.background = "red";
-    } else {
-        e.target.style.background = "blue";
-    }
-}
-
-function changePlayer() {
-    if (currentPlayer == "player1") {
-        currentPlayer = "player2";
-    } else {
-        currentPlayer = "player1";
-    }
-    gameBoard;
-}
