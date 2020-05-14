aa190bfe83cdeb729a043a5de82b4addc5700906
mvangin <43646116+mvangin@users.noreply.github.com>
2020-05-14 00:28:52 -0700

    playerButton appear/hide at start and end of game

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,7 @@ var gameBoard = (function () {
     const winner = document.querySelector('.winner');
     const player1Div = document.querySelector('.player1');
     const player2Div = document.querySelector('.player2');
+    const playerDivs = document.querySelectorAll('.player');
 
 
     var gameBoard = ["", "", "",
@@ -16,7 +17,7 @@ var gameBoard = (function () {
         return { playerName, playerSymbol };
     }
 
-  
+
     const player1 = player("player1", "X");
     const player2 = player("player2", "0");
 
@@ -43,7 +44,6 @@ var gameBoard = (function () {
     }
 
 
-
     function determineWinner() {
 
         let players = [player1, player2];
@@ -59,6 +59,10 @@ var gameBoard = (function () {
                 ((gameBoard[0] + gameBoard[4] + gameBoard[8]) == winningCombo) ||
                 ((gameBoard[2] + gameBoard[4] + gameBoard[6]) == winningCombo)) {
                 winner.textContent = player.playerName + " is the winner!";
+                playerDivs.forEach((item) => {
+                    item.style.display = "none"
+                });
+
                 startGame.style.display = "inline";
                 removePress();
 
@@ -66,6 +70,9 @@ var gameBoard = (function () {
         });
 
         if (!gameBoard.includes("") && winner.textContent == "") {
+            playerDivs.forEach((item) => {
+                item.style.display = "none"
+            });
             winner.textContent = "Its a tie!";
             startGame.style.display = "inline";
             removePress();
@@ -73,72 +80,76 @@ var gameBoard = (function () {
     }
 
 
-        function clearGame() {
-            gameBoard = ["", "", "",
-                "", "", "",
-                "", "", ""];
-            currentPlayer = player1;
-            winner.textContent = "";
-            updateGameBoard();
-        }
-
-
-
-        startGame.addEventListener("click", () => {
-            startGame.style.display = "none";
-            clearGame();
-            playerPress();
-
+    function initializeGame() {
+        gameBoard = ["", "", "",
+            "", "", "",
+            "", "", ""];
+        startGame.style.display = "none";
+        playerDivs.forEach((item) => {
+            item.style.display = "flex"
         });
+        currentPlayer = player1;
+        player1Div.classList.add("currentPlayer");
+        winner.textContent = "";
+        updateGameBoard();
+        playerPress();
+    }
 
 
-        function changePlayer() {
+
+    startGame.addEventListener("click", () => {
+        initializeGame();
+
+    });
+
+
+    function changePlayer() {
+        if (currentPlayer == player1) {
+            currentPlayer = player2;
+            player1Div.classList.add("currentPlayer");
+            player2Div.classList.remove("currentPlayer");
+
+        } else {
+            currentPlayer = player1;
+            player2Div.classList.add("currentPlayer");
+            player1Div.classList.remove("currentPlayer");
+        }
+    }
+
+    function playerPress() {
+        boardSquares.forEach((item) => {
+            item.addEventListener("click", playerEntry, true);
+        });
+    }
+
+    function removePress() {
+        boardSquares.forEach((item) => {
+            item.removeEventListener("click", playerEntry, true);
+
+        })
+    }
+
+
+    function playerEntry(e) {
+
+        let index = e.target.dataset.gridValue;
+        if (gameBoard[index] == "") {
             if (currentPlayer == player1) {
-                currentPlayer = player2;
-                player1Div.classList.add("currentPlayer");
-                player2Div.classList.remove("currentPlayer");
-
+                gameBoard[index] = "X";
             } else {
-                currentPlayer = player1;
-                player2Div.classList.add("currentPlayer");
-                player1Div.classList.remove("currentPlayer");
+                gameBoard[index] = "0";
             }
-        }
-
-        function playerPress() {
-            boardSquares.forEach((item) => {
-                item.addEventListener("click", playerEntry, true);
-            });
-        }
-
-        function removePress() {
-            boardSquares.forEach((item) => {
-                item.removeEventListener("click", playerEntry, true);
-
-            })
-        }
-
-
-        function playerEntry(e) {
-
-            let index = e.target.dataset.gridValue;
-            if (gameBoard[index] == "") {
-                if (currentPlayer == player1) {
-                    gameBoard[index] = "X";
-                } else {
-                    gameBoard[index] = "0";
-                }
-                changePlayer();
-
-
-            }
-            updateGameBoard(index);
-            determineWinner();
+            changePlayer();
 
 
         }
+        updateGameBoard(index);
+        determineWinner();
 
 
-    } ());
+    }
+
+
+}());
 
 
